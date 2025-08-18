2a9b8060ad0b11a5fc85e6f3a17af18742961062
hungquan99 <142434237+hungquan99@users.noreply.github.com>
2025-08-18 09:37:05 +0700

    Update and rename weaponCategories.lua to weaponCategories.json

--- API/VoxSeas/weaponCategories.lua
+++
@@ -1,6 +0,0 @@
-return {
-    melee = {"Basic Style","Water Kung-Fu","Black Leg","Electric Claw"},
-    sword = {"Dual Dark Blade", "Dark Blade","Divine Pole","Bisento","Soul Cane","Dual Blade","Trident","Shadow Blade","Iron Mace","Dual Katana","Cutlass","Katana"},
-    gun = {"CyBurst Seven","Musket","Magma Gun","Flintlock","Slingshot"},
-    fruit = {"Venom"}
-}

610ad8c8f3e291ac74a6516b70ad4d3e148ffc20
hungquan99 <142434237+hungquan99@users.noreply.github.com>
2025-08-18 09:33:13 +0700

    Create weaponCategories.lua

---
+++ API/VoxSeas/weaponCategories.lua
@@ -0,0 +1,6 @@
+return {
+    melee = {"Basic Style","Water Kung-Fu","Black Leg","Electric Claw"},
+    sword = {"Dual Dark Blade", "Dark Blade","Divine Pole","Bisento","Soul Cane","Dual Blade","Trident","Shadow Blade","Iron Mace","Dual Katana","Cutlass","Katana"},
+    gun = {"CyBurst Seven","Musket","Magma Gun","Flintlock","Slingshot"},
+    fruit = {"Venom"}
+}

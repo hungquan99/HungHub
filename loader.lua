b33b5d3e0ebdef82c173f8cd988d28c06748e51e
hungquan99 <142434237+hungquan99@users.noreply.github.com>
2026-07-30 08:38:42 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,8 @@
 if getgenv().AutoExecute == true then
     Auto_Execute = true
 end
-loadstring(game:HttpGet("https://vss.pandauth.com/virtual/file/6b67b72c4a68473f"))()
-loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/c7a733a4a6874dac"))()
+if game.PlaceId == 84515722934860 then
+    loadstring(game:HttpGet("https://vss.pandauth.com/virtual/file/6b67b72c4a68473f"))()
+else
+    loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/c7a733a4a6874dac"))()
+end

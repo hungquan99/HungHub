26337a43c66e256625101175dc3855d185e3e7c6
hungquan99 <142434237+hungquan99@users.noreply.github.com>
2026-07-30 08:36:36 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if getgenv().AutoExecute == true then
     Auto_Execute = true
 end
-
+loadstring(game:HttpGet("https://vss.pandauth.com/virtual/file/6b67b72c4a68473f"))()
 loadstring(game:HttpGet("https://vss.pandadevelopment.net/virtual/file/c7a733a4a6874dac"))()
